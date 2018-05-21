43f0279b96f3f9aa0bd73b022b3100b0ed2ea055
Luke Worley <luke.worley@colorado.edu>
2018-05-21 23:42:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
+  if !position_taken?(board,index) && index.between?(0,8)
     return true
   else
     return false
